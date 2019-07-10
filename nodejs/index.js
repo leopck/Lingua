f891cee37f9d464efa9a9e7dce9061f011ecf188
Stanley Phoong <stanley.phoong@gmail.com>
2019-07-11 00:42:23 +0800

    Python & Nodejs integration
    Created Nodejs Callback
    Moved python to another dir
    
    Signed-off-by: Stanley Phoong <stanley.phoong@gmail.com>

---
+++ nodejs/index.js
@@ -0,0 +1,74 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+var user = {
+   "user4" : {
+      "name" : "mohit",
+      "password" : "password4",
+      "profession" : "teacher",
+      "id": 4
+   }
+}
+
+app.post('/reply', function (req, res) {  
+   // console.log(res);
+   console.log(req.body);
+   
+   // data = JSON.parse(res);
+   // console.log(data);
+   
+   res.end( "JSON.stringify(data)" );
+   // fs.writeFile('reply.json', 'Simply Easy Learning!', function(err) {
+   //    if (err) {
+   //       return console.error(err);
+   //    }
+   //    console.log("Data written successfully!");
+   //    console.log("Let's read newly written data");
+   // });
+})
+
+app.get('/listUsers', function (req, res) {
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+      console.log( data );
+      res.end( data );
+   });
+})
+
+app.get('/addUser', function (req, res) {
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+      data = JSON.parse( data );
+      data["user4"] = user["user4"];
+      console.log( data );
+      res.end( JSON.stringify(data));
+   });
+})
+
+app.get('/:id', function (req, res) {
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+      var users = JSON.parse( data );
+      var user = users["user" + req.params.id] 
+      console.log( user );
+      res.end( JSON.stringify(user));
+   });
+})
+
+app.get('/deleteUser/:id', function (req, res) {
+   // First read existing users.
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+      data = JSON.parse( data );
+      delete data["user" + req.params.id];
+       
+      console.log( data );
+      res.end( JSON.stringify(data));
+   });
+})
+
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
