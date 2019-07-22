6cf66154be929f8cd64bdda9fa7a6fc010151c60
Stanley Phoong <stanley.phoong@gmail.com>
2019-07-23 00:44:09 +0800

    Removed samples
    Signed-off-by: Stanley Phoong <stanley.phoong@gmail.com>

--- python/models.py
+++
@@ -1,6 +0,0 @@
-class Models:
-    def __init__(self, arg):
-        self.arg = arg
-
-    def  (self, parameter_list):
-        pass
--- python/server.py
+++
@@ -1,6 +0,0 @@
-class LinguaServer:
-    def __init__(self, arg):
-        self.arg = arg
-
-    def createCallback(self, parameter_list):
-        pass
